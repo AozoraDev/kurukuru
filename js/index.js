e141fab986fd5130d452a666616c337155d0ce89
AozoraDev <niicoremusic@gmail.com>
2023-07-04 03:31:22 +0800

    Some WebSocket thingy

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-let img = document.createElement("img");
+const img = document.createElement("img");
 img.src = "img/herta-kurukuru.gif";
 img.alt = "Herta Kurukuru";
 img.width = 240;
@@ -7,14 +7,16 @@ const formatter = new Intl.NumberFormat();
 const worker = new Worker("js/worker.js");
 
 const container = document.querySelector(".container");
-const data = document.querySelector(".data");
 const loading = document.querySelector(".loading");
+const websocket = document.querySelector(".websocket");
+const message = loading.querySelector("span");
 
 let totalCounts;
 let isTrusted;
 let timeout;
 
 // Apply settings
+message.innerText = "Applying settings...";
 settings.forEach(function (setting, index) {
     for (const key in setting) {
         const id = `#settings${index}[name="${key}"]`;
@@ -26,43 +28,109 @@ settings.forEach(function (setting, index) {
 
 worker.onerror = function (e) {
     console.error(e.message);
-    
     return;
 }
 
-let message = loading.querySelector("span");
-let header = loading.querySelector("h2");
-let total = data.querySelector("h1");
+const header = loading.querySelector("h2");
+const total = document.querySelector(".data h1");
+const ping = websocket.querySelector("#ping small");
+const pingIcon = document.querySelector("#ping i");
+const totalPlayers = document.querySelector("#online small");
+
+const usewebsocket = settings[1].usewebsocket;
 worker.onmessage = function (e) {
-    // Error if API is dead
-    if (e.data == "onloadError") {
-        header.innerText = "Oops!";
-        message.style.display = "block";
-    }
-    
-    // Add total counts and hide the loading
-    else if (typeof e.data == "number") {
-        totalCounts = e.data;
-        total.innerText = formatter.format(totalCounts);
+    switch (e.data.code) {
+        // -100: Error when no bitches, i mean... when failed to connecting to WebSocket or API
+        // Error if API is dead or something
+        case -100:
+            header.innerText = "Oops!";
+            message.innerHTML = "It seems that an error occurred while " + ((usewebsocket) ? "connecting to the server" : "retrieving data from the database") + ".<br />Please try again later.";
+            loading.classList.remove("loaded");
+            container.removeEventListener("click", onclick);
+            
+            if (usewebsocket) {
+                pingIcon.className = "bx bx-wifi-off";
+                pingIcon.style.color = "#FFF";
+                ping.innerText = "0ms";
+                totalPlayers.innerText = "0";
+            }
+        break;
         
-        setTimeout(function() {
-            loading.classList.add("loaded");
-        }, 1000);
-    }
-    
-    // Error during post user counts to database
-    else if (e.data == "updateError") {
-        console.error("Your current counts is not saved to the database due to error connecting to API");
-    }
-    
-    // Error when unauthorized
-    else if (e.data == "updateUnauthorized") {
-        console.error("Your current counts is not saved to the database due to unauthorized request to API");
-    }
-    
-    // ...
-    else if (e.data == "monika") {
-        executeMonika();
+        // 100: Connection estabilashed and initiation successfully
+        // Add total counts and hide the loading element.
+        // If websocket, add token to session storage and some stuff.
+        case 100:
+            totalCounts = e.data.totalCounts;
+            if (e.data.totalPlayers) totalPlayers.innerText = e.data.totalPlayers;
+            if (sessionStorage.getItem("token")) sessionStorage.removeItem("token");
+            if (e.data.token) sessionStorage.setItem("token", e.data.token);
+            total.innerText = formatter.format(totalCounts);
+            message.innerText = "Starting...";
+            
+            setTimeout(function() {
+                loading.classList.add("loaded");
+            }, 1000);
+        break;
+        
+        // 101 (WebSocket Only): Current counts is saved
+        // Change the total counts and online players
+        case 101:
+            total.innerText = formatter.format(e.data.totalCounts);
+            totalPlayers.innerText = e.data.totalPlayers;
+        break;
+        
+        // 99 (WebSocket Only): Connected to the WebSocket but not initiated yet
+        // In websocket, send message if connection estabilashed
+        case 99:
+            message.innerText = "Connected! Waiting for server response...";
+        break;
+        
+        // 95: Your currents counts cannot be saved because... unauthorized
+        case 95:
+            const msg95 = "Your current counts is not saved to the database due to unauthorized request to API";
+            notif(msg95);
+        break;
+        
+        // 94: Like code 95, but this time because the API is dead or something
+        case 94:
+            const msg94 = "Your current counts is not saved to the database due to error connecting to API";
+            notif(msg94);
+        break;
+        
+        // 93: Autoclick detected
+        case 93:
+            const msg93 = "Autoclick detected! Your current counts is not saved.";
+            notif(msg93);
+        break;
+        
+        // 1000 (WebSocket Only): Pong received
+        // In websocket, change ping every a sec
+        case 1000:
+            const latency = (e.data.ping > 999) ? 999 : e.data.ping;
+            let color = "#FFF"
+            let icon = "bx bx-wifi-off";
+            
+            if (latency > 700) {
+                icon = "bx bx-wifi-1";
+                color = "#FF0000";
+            } else if (latency > 300) {
+                icon = "bx bx-wifi-2";
+                color = "#FFFF00";
+            } else {
+                icon = "bx bx-wifi";
+                color = "#00FF00";
+            }
+            
+            pingIcon.className = icon;
+            pingIcon.style.color = color;
+            ping.innerText = latency + "ms";
+        break;
+        
+        // DEBUG STUFF
+        // Nice number, btw
+        case 69420:
+            console.log(e.data.data);
+        break;
     }
 }
 
@@ -70,12 +138,13 @@ window.onload = function() {
     if (localStorage.getItem("blocked") == "true") {
         header.innerText = "Oops!";
         message.innerText = "You are blocked from using this service.";
-        message.style.display = "block";
         
         return;
     }
     
-    worker.postMessage("onload");
+    if (!usewebsocket) websocket.style.display = "none";
+    message.innerText = (usewebsocket) ? "Connecting..." : "Retrieving data...";
+    worker.postMessage({ code: 100, usewebsocket: usewebsocket });
 }
 
 function startTimeout() {
@@ -96,17 +165,37 @@ function closePopup(id) {
 
 function updateSettings(ele) {
     const index = Number(ele.id.slice(8));
+    if (!settings[index]) settings[index] = {};
     settings[index][ele.name] = ele.checked;
     
     localStorage.setItem("settings", JSON.stringify(settings));
 }
 
+const notification = document.querySelector(".notification");
+container.removeChild(notification); // No more dummy element
+function notif(message) {
+    let clonedNotif = notification.cloneNode(true);
+    container.appendChild(clonedNotif);
+    
+    let notifMsg = clonedNotif.querySelector("span");
+    notifMsg.innerText = message;
+    setTimeout(function() {
+        clonedNotif.classList.add("show");
+    }, 100);
+    setTimeout(function() {
+        clonedNotif.classList.remove("show");
+        setTimeout(function() {
+            container.removeChild(clonedNotif);
+        }, 500)
+    }, 4100);
+}
+
 let sfxPos = 0;
 let tempCounts = 0;
 let userCounts = 0
-let user = data.querySelector("span");
+const user = document.querySelector(".data span");
 const s0Final = settings[0].kurukuru || settings[0].kururin;
-container.addEventListener("click", function(e) {
+function onclick(e) {
     if (timeout) clearTimeout(timeout);
     startTimeout();
     
@@ -128,13 +217,14 @@ container.addEventListener("click", function(e) {
     setTimeout(function() {
         container.removeChild(clonedImg);
     }, 800);
-});
+}
 
+container.addEventListener("click", onclick);
 setInterval(function() {
     if (!isTrusted) return;
     
     const counts = tempCounts;
     tempCounts = 0;
     
-    worker.postMessage({ counts: counts, isTrusted: isTrusted });
+    worker.postMessage({ code: 98, usewebsocket: usewebsocket, counts: counts, isTrusted: isTrusted });
 }, 5000);
--- js/worker.js
+++ js/worker.js
@@ -1,39 +1,116 @@
+let ws;
+let pingWorker;
+let ping = Date.now();
+
+const http = "https://";
+const origin = "api.aozora.my.id";
+
 onmessage = function (e) {
-    if (e.data == "onload") {
-        fetch("https://api.aozora.my.id/kurukuru")
-        .then(res => res.json())
-        .then(res => {
-            if (res.error) {
-                postMessage("onloadError")
-                return;
+    if (e.data.code == 100) {
+        if (e.data.usewebsocket) {
+            ws = new WebSocket("wss://" + origin + "/kurukuru/ws");
+            
+            ws.onerror = function(e) {
+                postMessage({ code: -100 });
+                clearInterval(pingWorker);
             }
             
-            postMessage(res.counts);
-        })
-        .catch(err => postMessage("onloadError"));
-    }
-    
-    else if (e.data.counts && e.data.isTrusted) {
-        fetch("https://api.aozora.my.id/kurukuru/update", {
-            method: "post",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                counts: e.data.counts,
-                isTrusted: e.data.isTrusted
-            })
-        })
-        .then(res => res.json())
-        .then(res => {
-            if (res.error) {
-                if (res.autoclick) {
-                    postMessage("monika");
+            ws.onopen = function(e) {
+                postMessage({ code: 99 });
+                pingWorker = setInterval(function() {
+                    ping = Date.now();
+                    ws.send("ping");
+                }, 1000);
+            }
+            
+            ws.onclose = function(e) {
+                postMessage({ code: -100 });
+                clearInterval(pingWorker);
+            }
+            
+            ws.onmessage = function(e) {
+                const response = (e.data == "pong") ? "pong" : JSON.parse(e.data);
+                
+                //if (response != "pong") postMessage({ code: 69420, data: response }); // Debug only
+                
+                // Ping latency
+                if (response == "pong") {
+                    postMessage({ code: 1000, ping: Date.now() - ping }); // Debug only
+                }
+                
+                else if (!response.error) {
+                    switch (response.code) {
+                        // Connection established
+                        case 0:
+                            postMessage({ code: 100, totalPlayers: response.totalPlayers, totalCounts: response.totalCounts, token: response.token });
+                        break;
+                        
+                        // Current counts is saved
+                        case 1:
+                            postMessage({ code: 101, totalPlayers: response.totalPlayers, totalCounts: response.totalCounts });
+                        break;
+                    }
+                }
+                
+                else if (response.error) {
+                    switch (response.code) {
+                        // Unauthorized
+                        case 2:
+                            postMessage({ code: 95 });
+                        break;
+                        
+                        // API breaks
+                        case 3:
+                            postMessage({ code: 94 });
+                        break;
+                        
+                        // Autoclick detected
+                        case 4:
+                            postMessage({ code: 93 });
+                        break;
+                    }
+                }
+            }
+        } else {
+            fetch(http + origin + "/kurukuru")
+            .then(res => res.json())
+            .then(res => {
+                if (res.error) {
+                    postMessage({ code: -100 })
                     return;
                 }
-                postMessage("updateUnauthorized");
-            }
-        })
-        .catch(err => postMessage("updateError"));
+                
+                postMessage({ code: 100, totalCounts: res.counts });
+            })
+            .catch(err => postMessage({ code: -100 }));
+        }
+    }
+    
+    else if (e.data.code == 98) {
+        if (e.data.usewebsocket) {
+            ws.send(JSON.stringify({ counts: e.data.counts, isTrusted: e.data.isTrusted }));
+        } else {
+            fetch(http + origin + "/kurukuru/update", {
+                method: "post",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    counts: e.data.counts,
+                    isTrusted: e.data.isTrusted
+                })
+            })
+            .then(res => res.json())
+            .then(res => {
+                if (res.error) {
+                    if (res.autoclick) {
+                        postMessage({ code: 93 });
+                        return;
+                    }
+                    postMessage({ code: 95 });
+                }
+            })
+            .catch(err => postMessage({ code: 94 }));
+        }
     }
 }
